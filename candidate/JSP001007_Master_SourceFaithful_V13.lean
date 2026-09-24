204eddd751b7c7b981c69e6f3327ba8a5ab04df2
e66812162-Liu <e66812162@gmail.com>
2026-09-23 23:44:38 -0400

    Add V13 candidate master

---
+++ candidate/JSP001007_Master_SourceFaithful_V13.lean
@@ -0,0 +1,8 @@
+import JSP001007_WP4Q_SourceFaithful_FinalWrapper_V13
+
+namespace JSP001007
+
+#check erdos1202_negative_public_statement_v13
+#print axioms erdos1202_negative_public_statement_v13
+
+end JSP001007
